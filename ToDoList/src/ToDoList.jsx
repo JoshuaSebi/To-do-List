b6c69ae40024997d1d943043e08aea415d8c4426
Joshua <joshua.palokaran@gmail.com>
2025-06-04 12:51:02 +0530

    Functions updated

--- ToDoList/src/ToDoList.jsx
+++ ToDoList/src/ToDoList.jsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 const ToDoList = () => {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
+    const [completedTasks, setCompletedTasks] = useState([]);
 
     const handleInputChange = (e) => {
         setNewTask(e.target.value);
@@ -17,24 +18,37 @@ const ToDoList = () => {
         }    
     }
 
+    const handleCompletedTasks = (index) => {
+        const taskToComplete = tasks[index];
+        setCompletedTasks([...completedTasks, taskToComplete]);
+        setTasks(tasks.filter((_,i) => i !== index));
+    }
+
     const handleDeleteTask = (index) => {
         const updatedTasks = tasks.filter((_,i)=> i!== index);
         setTasks(updatedTasks);
     }
 
+    const handleEnterKey = (e) => {
+        if (e.key === 'Enter') {
+            handleAddTask();
+        }
+    }
+
   return (
     <div className='to-do-list'>
-        <h1>To-Do-List</h1>
-        <div>
+        <h1>To-Do-List.</h1>
+        <div className='input-container'>
             <input 
                 type="text" 
                 value={newTask} 
-                onChange={handleInputChange} 
+                onChange={handleInputChange}
+                onKeyDown={handleEnterKey} 
                 placeholder='Enter new Task...'/>
             <button 
                 className='add-button' 
                 onClick={handleAddTask}>
-                    +
+                  <ion-icon name="add-outline"></ion-icon>
                 </button>
         </div>
         
@@ -45,11 +59,20 @@ const ToDoList = () => {
                     <button 
                         className='delete-button' 
                         onClick={() => handleDeleteTask(index)}>
-                            x
+                            <ion-icon name="trash-outline"></ion-icon>
+                    </button>
+                    <button onClick={()=>handleCompletedTasks(index)} >
+                        completed
                     </button>
                 </li>
             )}
         </ol>
+        <h2>Completed Tasks</h2>
+        <ul>
+            {completedTasks.map((task, index) => (
+                <li key={index}>{task}</li>
+            ))}
+        </ul>
     </div>
   )
 }
